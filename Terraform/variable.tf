95ea0724e907f787904bebdffc5c09ed24c005a9
rajdhage2104 <rajdhage2000@gmail.com>
2025-02-21 10:54:44 +0530

    CI-CD workflow and terraform file updated

---
+++ Terraform/variable.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami" {
+  description = "Ami for the instance"
+  type        = string
+}
+ 
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+ 
+variable "availability_zone" {
+  description = "AWS Availability Zone"
+  type        = string
+}
+ 
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR block"
+  type        = string
+}
+ 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+ 
+variable "ssh_public_key" {
+  description = "Path to SSH public key"
+  type        = string
+}
