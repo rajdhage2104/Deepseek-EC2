d6c3c0cb40279a0f6ab424c8a9e677a3de23efc9
rajdhage2104 <144928213+rajdhage2104@users.noreply.github.com>
2025-02-24 13:29:25 +0530

    Update userdata.sh

--- Terraform/userdata.sh
+++ Terraform/userdata.sh
@@ -2,19 +2,21 @@
 dnf update -y
 
 dnf install -y amazon-linux-extras
-amazon-linux-extras enable java-openjdk17
+amazon-linux-extras enable corretto17
+sleep 5
 dnf install -y \
     yum-utils \
     apt-transport-https \
     ca-certificates \
     curl \
-    openjdk-17-jre \
+    java-17-amazon-corretto \
     gcc \
-    kernel-devel-$(uname -r)
+    kernel-devel \
+    kernel-headers
 
 aws s3 cp --recursive s3://ec2-linux-nvidia-drivers/latest/ .
 chmod +x NVIDIA-Linux-x86_64*.run
-/bin/sh ./NVIDIA-Linux-x86_64*.run --tmpdir . --silent
+/bin/sh ./NVIDIA-Linux-x86_64*.run --tmpdir . --silent --logfile /var/log/nvidia-installer.log
 
 echo "options nvidia NVreg_EnableGpuFirmware=0" | tee --append /etc/modprobe.d/nvidia.conf
 
@@ -22,8 +24,9 @@ dnf install -y docker
 usermod -aG docker ec2-user
 systemctl enable --now docker
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+dnf config-manager --add-repo https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 dnf install -y nvidia-container-toolkit
 nvidia-ctk runtime configure --runtime=docker
 systemctl restart docker
+
+reboot
